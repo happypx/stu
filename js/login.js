7ffe23d3b4c0d001ca20889b69ca8f23d4ca56b0
happypx <105258438+happypx@users.noreply.github.com>
2022-06-10 19:52:34 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,30 +1,43 @@
 const uname = document.querySelector('[name=username]')
-    const pwd = document.querySelector('[name=password]')
-    const data = sessionStorage.userInfo
-    const arr = data ? JSON.parse(data) : []
-    const form = document.querySelector('form')
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const my_checkbox = document.querySelector('#my-checkbox')
-      if (my_checkbox.checked) {
-        let flag = false
-        if (arr.length) {
-          for (let i = 0; i < arr.length; i++) {
-            if (uname.value == arr[i].uname) {
-              if (pwd.value == arr[i].pwd) {
-                flag = true
-              }
-            }
+const pwd = document.querySelector('[name=password]')
+const data = sessionStorage.userInfo
+const arr = data ? JSON.parse(data) : []
+const form = document.querySelector('form')
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const my_checkbox = document.querySelector('#my-checkbox')
+  if (my_checkbox.checked) {
+    let flag = false
+    if (arr.length) {
+      for (let i = 0; i < arr.length; i++) {
+        if (uname.value == arr[i].uname) {
+          if (pwd.value == arr[i].pwd) {
+            flag = true
           }
         }
-        if (flag) {
-          alert('登录成功')
-          sessionStorage.loginUser=uname.value
-          location.href = './index.html'
-        } else {
-          alert('用户名或密码错误')
-        }
-      }else{
-        alert('请先同意协议')
       }
-    })
+    }
+    if (flag) {
+      alert('登录成功')
+      sessionStorage.loginUser = uname.value
+      location.href = './index.html'
+    } else {
+      alert('用户名或密码错误')
+    }
+  } else {
+    alert('请先同意协议')
+  }
+})
+
+const tab_nav=document.querySelector('.tab-nav')
+tab_nav.addEventListener('click',function(e){
+  if (e.target.tagName==="A") {
+    document.querySelector('.tab-nav a.active').classList.remove('active')
+    e.target.classList.add('active')
+    const tab_pane=document.querySelectorAll('.tab-pane')
+    for(let i=0;i<tab_pane.length;i++){
+      tab_pane[i].style.display='none'
+    }
+    tab_pane[e.target.dataset.id].style.display='block'
+  }
+})
